76eade0fb6ec843390b88131a4a56a6de610013e
Stephanie Djidjev <sdjidjev@gmail.com>
2014-10-03 23:22:28 -0700

    made it more random

--- completed_code/js/replace.js
+++ completed_code/js/replace.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	var categories = ['', '/people', '/food', '/animals', '/nature','/cats'];
-	replaceWithKittens();
+	replaceWithPicture();
 	$(document).bind('DOMSubtreeModified', function () {
-		replaceWithKittens();
+		replaceWithPicture();
 	});
 	function getCategory() {
 		var i = Math.floor(Math.random()*categories.length);
@@ -16,11 +16,16 @@ $(document).ready(function(){
 				$picture.ready(function() {
 					var w = Math.round($picture.width());
 					var h = Math.round($picture.height());
+					var scalar = Math.random()*1 + 1.0;
+					var new_w = Math.round(scalar*w);
+					var new_h = Math.round(scalar*h);
 					if ($picture.attr('src') && $picture.attr('src').indexOf('lorempixel') == -1) {
-						$picture.attr('src','http://lorempixel.com/'+w+'/'+h+getCategory());
+						$picture.attr('src','http://lorempixel.com/'+new_w+'/'+new_h+getCategory());
 					} else if (!$picture.attr('src')) {
-						$picture.attr('src','http://lorempixel.com/'+w+'/'+h+getCategory());
+						$picture.attr('src','http://lorempixel.com/'+new_w+'/'+new_h+getCategory());
 					}
+					$picture.width(w+'px');
+					$picture.height(h+'px');
 					
 				});
 			}
